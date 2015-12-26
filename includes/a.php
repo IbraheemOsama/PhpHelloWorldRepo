f10c2054c988ea914d074b72d623993f662925de
IbraheemOsama <Ibraheem.osama@gmail.com>
2015-12-26 18:40:20 +0200

    commiting hello world

---
+++ includes/a.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+</head>
+
+<body>
+    
+    <?php
+//phpinfo();
+?>
+
+<?php
+$greeter="Hello MDC from a";
+echo($greeter);
+?>
+    
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+	$page="";
+		if(isset($_POST['name'])) {
+			$page = 'thanks';
+		}
+
+?>
+
+
+<html>
+
+<head>
+</head>
+
+<body>
+    
+    <?php
+    echo($page);
+//phpinfo();
+?>
+
+<h2>
+<?php
+
+include("includes\a.php");
+
+$querystring=$_GET['id'];
+
+echo($querystring);
+?>
+</h2>
+
+<!--<?php for($i=0;$i<100;$i++): ?>
+<h3>Hello MDC</h3>
+
+<?php endfor ?>-->
+    
+    
+    
+    <?php if($querystring=="1"):?>
+    <span>one</span>
+    <?php endif ?>
+    
+    
+   <form method="post">
+	<p>
+		<label for="name">Name</label>
+		<input type="text" name="name" id="name" placeholder="Name" />
+	</p>
+
+	<p>
+		<input type="radio" name="attending" value="yes" /> I&rsquo;m coming! <br />
+		<input type="radio" name="attending" value="no" /> I can&rsquo;t make it.
+
+	</p>
+
+	<p>
+		<label for="guests">Date&rsquo;s Name (if applicable)</label>
+		<input type="text" name="date_name" id="date_name" placeholder="Guest Name" />
+	</p>
+
+	<input type="submit" value="RSVP" class="submit" />
+</form>
+
+</body>
+
+</html>
