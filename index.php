120e81ca3b211f043bdd9cb4605c24565488a80e
IbraheemOsama <Ibraheem.osama@gmail.com>
2015-12-28 20:13:07 +0200

    add sql azure

--- index.php
+++ index.php
@@ -2,46 +2,67 @@
 
 //require __DIR__ . '/Crassaert\AzureDocumentDB\AzureDocumentDB.php';
 //require_once 'includes\AzureDocumentDB.php';
-use Crassaert\AzureDocumentDB\AzureDocumentDB;
 
-	$page="";
-		if(isset($_POST['name'])) {
-			$page = 'thanks';
-		}
-        
-        
-         $host = 'https://phpdocumentdbtest.documents.azure.com:443/';
-    $master_key = "+GwNhmxQT2pf1ux8r4WGV3tqkUyidxKmiHPq1RMkuYoKgHJnSBeVPmgljGD3EfcYTySypJ4+TEFnR//MrFfcgQ==";//getenv("documentDBKey"); ;
-    
-    // connect DocumentDB
-   $db = new AzureDocumentDB($host, $master_key, false);
 
-   $db->get('database')->create('clothes');
-    $dbs=$db->get('database')->_list();
+$page="";
+if(isset($_POST['name'])) {
+    $page = 'thanks';
+}
+
+
+$host = "tcp:f6mooov6xr.database.windows.net,1433";
+$user = "FlyDBAdmin";
+$pwd = "P@ssw0rd123@";
+$db = "DynamicAppsBase";
+try{
+    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if($conn == false)
+    die(FormatErrors(sqlsrv_errors()));
+    
+    $tsql = "select * from [TablesValidation]";
+    $getProducts = sqlsrv_query($conn, $tsql);
+    if ($getProducts == FALSE)
+        die(FormatErrors(sqlsrv_errors()));
+    
+    while($row = sqlsrv_fetch_array($getProducts, SQLSRV_FETCH_ASSOC))
+    {
+        echo($row['ID']);
+        echo("<br/>");
+    }
+    sqlsrv_free_stmt($getProducts);
+    sqlsrv_close($conn);
     
     
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+
 ?>
 
 
-<html>
+  <html>
 
-<head>
-</head>
+  <head>
+  </head>
 
-<body>
-    
+  <body>
+
     <?php
-    echo($page);
-    echo($master_key);
-    foreach($dbs as $key=>$value)
-    {
-     echo($value);   
-    }
-    
+echo($page);
+echo($master_key);
+foreach($dbs as $key=>$value)
+{
+    echo($value);
+}
+
 //phpinfo();
 ?>
 
-<h2>
+      <h2>
 <?php
 
 include("includes\a.php");
@@ -52,38 +73,39 @@ echo($querystring);
 ?>
 </h2>
 
-<!--<?php for($i=0;$i<100;$i++): ?>
+      <!--<?php for($i=0;$i<100;$i++): ?>
 <h3>Hello MDC</h3>
 
 <?php endfor ?>-->
-    
-    
-    
-    <?php if($querystring=="1"):?>
-    <span>one</span>
-    <?php endif ?>
-    
-    
-   <form method="post">
-	<p>
-		<label for="name">Name</label>
-		<input type="text" name="name" id="name" placeholder="Name" />
-	</p>
 
-	<p>
-		<input type="radio" name="attending" value="yes" /> I&rsquo;m coming! <br />
-		<input type="radio" name="attending" value="no" /> I can&rsquo;t make it.
 
-	</p>
 
-	<p>
-		<label for="guests">Date&rsquo;s Name (if applicable)</label>
-		<input type="text" name="date_name" id="date_name" placeholder="Guest Name" />
-	</p>
+      <?php if($querystring=="1"):?>
+        <span>one</span>
+        <?php endif ?>
 
-	<input type="submit" value="RSVP" class="submit" />
-</form>
 
-</body>
+          <form method="post">
+            <p>
+              <label for="name">Name</label>
+              <input type="text" name="name" id="name" placeholder="Name" />
+            </p>
 
-</html>
+            <p>
+              <input type="radio" name="attending" value="yes" /> I&rsquo;m coming!
+              <br />
+              <input type="radio" name="attending" value="no" /> I can&rsquo;t make it.
+
+            </p>
+
+            <p>
+              <label for="guests">Date&rsquo;s Name (if applicable)</label>
+              <input type="text" name="date_name" id="date_name" placeholder="Guest Name" />
+            </p>
+
+            <input type="submit" value="RSVP" class="submit" />
+          </form>
+
+  </body>
+
+  </html>
