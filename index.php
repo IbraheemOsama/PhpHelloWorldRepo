998a43c262ad78fa387ca6211e42b29fec01c055
IbraheemOsama <Ibraheem.osama@gmail.com>
2015-12-30 09:45:45 +0200

    adding query

--- index.php
+++ index.php
@@ -3,19 +3,32 @@
 //require __DIR__ . '/Crassaert\AzureDocumentDB\AzureDocumentDB.php';
 //require_once 'includes\AzureDocumentDB.php';
 
-
-$page="";
-
-
 $host = "tcp:f6mooov6xr.database.windows.net,1433";
 $user = "FlyDBAdmin";
 
 // this method retrieves settings
 $pwd = getenv("databasePassword");
 $db = "OnlineShoppingDb";
+$conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+
+$page="";
+
+if(isset($_POST['name'])) {
+    
+    $name = $_POST['name'];
+    $sql_insert = "INSERT INTO [Orders] (Name) 
+                   VALUES (?)";
+    $stmt = $conn->prepare($sql_insert);
+    $stmt->bindValue(1, $name);
+    //$stmt->bindValue(2, $email);
+    //$stmt->bindValue(3, $date);
+    $stmt->execute();
+    
+    $page = 'thanks';
+}
 
 try{
-    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
+    
     $sql_select = "SELECT * FROM [Orders]";
     $stmt = $conn->query($sql_select);
     $registrants = $stmt->fetchAll();
@@ -38,19 +51,7 @@ catch(Exception $e){
 }
 
 
-if(isset($_POST['name'])) {
-    
-    $name = $_POST['name'];
-    $sql_insert = "INSERT INTO [Orders] (Name) 
-                   VALUES (?)";
-    $stmt = $conn->prepare($sql_insert);
-    $stmt->bindValue(1, $name);
-    //$stmt->bindValue(2, $email);
-    //$stmt->bindValue(3, $date);
-    $stmt->execute();
-    
-    $page = 'thanks';
-}
+
 
 echo "<h3>Table created.</h3>";
 
